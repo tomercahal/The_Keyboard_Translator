16eb6937334846a8f3803827b54c41185a1d1d41
tomer <Tomercahal@gmail.com>
2019-12-15 11:49:42 +0200

    Just uploding the javascript file

---
+++ Main.js
@@ -0,0 +1,12 @@
+// var dict = {'q': '/', 'w': '/', 'e': '/', 'r': '/', 't': '/', 'y': '/', 'u': '/', 'i': '/', 'o': '/', 'p': '/', 'q': '/', 'a': '/', 's': '/', 'd': '/', 'f': '/', 
+// 'g': '/', 'h': '/', 'j': '/', 'k': '/', 'l': '/', ';': '/', '\'': '/', 'q': '/', 'q': '/', 'q': '/', };
+
+// var dict_english_to_hebrew = {'a': 'ש', 'b':'נ'};
+
+// var name = readline.question; //This will be a long string containing the text that is in the wrong language
+
+// translated_string = "";
+// for(var letter in name)
+// {
+//     translated_string += dict_english_to_hebrew[letter];
+// }
